ac4ac229b378eda3e5a6e30c92932765c0c6d47c
e-evensen <elijah.evensen@gmail.com>
2022-11-10 13:36:38 -0500

    Update t-web.py

---
+++ t-web.py
@@ -0,0 +1,15 @@
+
+def list_projects():
+    return 1
+
+def view_project():
+    return 1
+
+def create_project():
+    return 1
+
+def edit_project():
+    return 1
+
+def delete_project():
+    return 1
