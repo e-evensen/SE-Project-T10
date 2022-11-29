160c977a4aea27a4158881aafff6cdd954c0b72f
BenLeaUNCC <108777057+BenLeaUNCC@users.noreply.github.com>
2022-11-29 14:03:24 -0500

    Added Logout function and button

--- t-web.py
+++ t-web.py
@@ -37,6 +37,13 @@ def list_projects():
 def login():
     return render_template('login.html')
 
+@app.route('/logout')
+def logout():
+    if session.get('user'):
+        session.clear()
+
+    return redirect(url_for('index'))
+
 @app.route('/register')
 def register():
     return render_template('register.html')
